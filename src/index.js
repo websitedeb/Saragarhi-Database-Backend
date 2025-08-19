632fc08b4111dd8d8c9cefddd943066640548729
paragrghoshal <ghoshsar000@students.centergrove.k12.in.us>
2025-08-19 18:59:57 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,169 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import bcrypt from 'bcryptjs'
+
+const app = new Hono()
+
+app.use('*', cors())
+
+app.put('/addUser', async (c) => {
+  const body = await c.req.json()
+  const { email, name, teamCode, Password } = body
+
+  if (!email || !name || !teamCode || !Password) {
+    return c.json({ success: false, error: 'All fields are required' }, 400)
+  }
+
+  const checkUser = await c.env.DB.prepare(
+    'SELECT * FROM Users WHERE Email = ?')
+    .bind(email)
+    .first();
+
+  const teamCheck = await c.env.DB.prepare(
+    'SELECT * FROM Teams WHERE "Team Code" = ?'
+  ).bind(teamCode).first();
+
+  if (checkUser) {
+    return c.json({ success: false, error: 'User already exists' }, 409)
+  }
+
+  if (!teamCheck) {
+    return c.json({ success: false, error: 'Team does not exist' }, 404)
+  }
+
+  const hashedPassword = await bcrypt.hash(Password, 10)
+
+  const result = await c.env.DB.prepare(
+    'INSERT INTO Users (Email, Name, "Team Code", Password) VALUES (?, ?, ?, ?)'
+    )
+    .bind(email, name, teamCode, hashedPassword)
+    .all();
+
+  if (result.error) {
+    return c.json({ success: false, error: 'Failed to add user' }, 500)
+  }
+  else{
+    return c.json({ success: true, result })
+  }
+});
+
+app.post('/getUser', async (c) => {
+  const { email, Password } = await c.req.json();
+  if (!email || !Password) {
+    return c.json({ success: false, error: 'Email and Password are required' }, 400)
+  }
+
+  const pw = await c.env.DB.prepare(
+    'SELECT * FROM Users WHERE Email = ?'
+  ).bind(email).first();
+
+  if (!pw) {
+    return c.json({ error: 'User not found' }, 404)
+  }
+
+  const isValid = await bcrypt.compare(Password, pw.Password);
+
+  if (!isValid) {
+    return c.json({ success: false, error: 'Invalid password' + pw.Password }, 401)
+  }
+  else{
+    const payload = {
+      Email: email,
+      TeamCode: pw['Team Code'],
+      Name: pw.Name,
+      Password: pw.Password
+    }
+
+    return c.json({ success: true, message: 'User authenticated successfully', data: payload });
+  }
+});
+
+app.post("/addTeam", async (c) => {
+  const body = await c.req.json();
+  const { teamCode, teamNum, teamName} = body;
+
+  if (!teamCode || !teamNum || !teamName) {
+    return c.json({ success: false, error: 'All fields are required' }, 400);
+  }
+  const result = await c.env.DB.prepare(
+    'INSERT INTO Teams ("Team Code", "Number", "Name", "DataSetOne", "DataSetTwo", "DataSetThree", "DataSetFour", "DataSetFive", "DataSetSix", "DataSetSeven", "DataSetEight", "DataSetNine", "DataSetTen") VALUES (?, ?, ?, "", "", "", "", "", "", "", "", "", "")'
+  ).bind(teamCode, teamNum, teamName).all();
+
+  if (result.error) {
+    return c.json({ success: false, error: 'Failed to add team' }, 500);
+  } else {
+    return c.json({ success: true, result });
+  }
+});
+
+app.post('/getTeam', async (c) => {
+  const { teamName } = await c.req.json();
+
+  if (!teamName) {
+    return c.json({ success: false, error: 'Team name is required' }, 400);
+  }
+
+  const team = await c.env.DB.prepare(
+    'SELECT * FROM Teams WHERE Name = ?'
+  ).bind(teamName).first();
+
+  if (!team) {
+    return c.json({ success: false, error: 'Team not found' }, 404);
+  }
+  else{
+    return c.json({ success: true, team });
+  }
+
+});
+
+app.post('/addReport', async c => {
+  const body = await c.req.json();
+  const { NumberOfDataSets, TeamNumber } = body;
+  
+  let sets = [];
+  let values = [];
+
+  const dataSetColumns = ["DataSetOne", "DataSetTwo", "DataSetThree", "DataSetFour", "DataSetFive", "DataSetSix", "DataSetSeven", "DataSetEight", "DataSetNine", "DataSetTen"];
+  const keys = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"];
+
+  for (let i = 0; i < NumberOfDataSets; i++) {
+    const key = keys[i];
+    const [type, value] = body[key]
+    const column = dataSetColumns[i];
+
+    switch (type) {
+      case "text":
+        sets.push(`${column} = ?`);
+        values.push(value);
+        break;
+      case "number":
+        sets.push(`${column} = ${column} + ?`);
+        values.push(value);
+        break;
+      case "avg":
+        sets.push(`${column} = (${column} + ?)/2`);
+        values.push(value);
+        break;
+    }
+  }
+  
+  const sql = `
+    UPDATE Teams
+    SET ${sets.join(", ")}
+    WHERE Number = ?
+  `;
+
+  values.push(TeamNumber);
+
+  const res = await c.env.DB.prepare(sql).bind(...values).run();
+
+  if (res.error) {
+    return c.json({ success: false, error: 'Internal Database Error' }, 500);
+  }
+  else{
+    console.log("Values:", ...values);
+    return c.json({ success: true, message: 'added new dataset(s)' }, 200);
+  }
+});
+
+export default app;
